87a0be16f261a0a93984cea131c5f9ec04bc4864
Tim Roetcisoender <timrritz@comcast.net>
2018-07-26 12:31:11 -0700

    commit after revert

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^/(?P<name>[a-zA-Z]+)$', views.index),
     url(r'^/prep/(?P<name>[a-zA-Z]+)$', views.prep_game),
-    url(r'^/getPlayerChar/(?P<plyrName>[a-zA-Z]+)$', views.get_player_char)
+    # url(r'^/getPlayerChar/(?P<plyrName>[a-zA-Z]+)$', views.get_player_char)
 ]
